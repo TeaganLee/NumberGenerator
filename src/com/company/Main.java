39989fc83d2f21d1fbe00825d069aafaedabfce1
TeaganLee20 <teagan.lee.2020@student.lhschools.org>
2020-04-02 19:48:40 -0400

    Conditionals

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,18 +1,24 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println((int)(Math.random()*100)+1);
-
-        if ( < 50){
-            System.out.println(¨large numbers¨);
-        }else if ( > 50){
-            System.out.println(¨Tiny number¨);
-        }else{
-            System.out.println(¨Your middling¨);
-        }
+        randomNumber();
 
     }
+    public static void randomNumber(){
+        System.out.println((int) (Math.random() * 100) + 1);
+
+
+    if ( int < 50){
+        System.out.println("Large number");
+    }else if ( int > 50){
+        System.out.println("Tiny number");
+    }else{
+        System.out.println("Your middling");
+    }
+
 }
