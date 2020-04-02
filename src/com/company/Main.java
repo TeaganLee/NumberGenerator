697cf6b68dece6e3c5641bfa7bef3a45f4451df1
TeaganLee20 <teagan.lee.2020@student.lhschools.org>
2020-04-02 10:37:21 -0400

    IntelliJ

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println((int)(Math.random()*100)+1);
+
+        if ( < 50){
+            System.out.println(¨large numbers¨);
+        }else if ( > 50){
+            System.out.println(¨Tiny number¨);
+        }else{
+            System.out.println(¨Your middling¨);
+        }
+
+    }
+}
